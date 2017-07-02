7c4742f98d1fde8e75aa8a2bdd9fcd84c6207fc7
riswanda <root.devilscream@gmail.com>
2017-07-02 15:10:21 +0000

    Add function for call a library from directory 'libs'

--- test/mellow.js
+++ test/mellow.js
@@ -131,3 +131,45 @@ class Validator {
     }
 
 }
+
+class Library {
+
+    alert(txt, title, button) {
+        var ALERT_TITLE = title;
+        var ALERT_BUTTON_TEXT = button;
+        var mObj,alertObj,h1,msg,btn;
+       
+        if(document.getElementById("modalContainer")) return;
+    
+        mObj = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+        mObj.id = "modalContainer";
+        mObj.style.height = document.documentElement.scrollHeight + "px";
+    
+        alertObj = mObj.appendChild(document.createElement("div"));
+        alertObj.id = "alertBox";
+        if(document.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+        alertObj.style.left = (document.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+        alertObj.style.visiblity="visible";
+    
+        h1 = alertObj.appendChild(document.createElement("h1"));
+        h1.appendChild(document.createTextNode(ALERT_TITLE));
+    
+        msg = alertObj.appendChild(document.createElement("p"));
+        msg.innerHTML = txt;
+    
+        btn = alertObj.appendChild(document.createElement("a"));
+        btn.id = "closeBtn";
+        btn.appendChild(document.createTextNode(ALERT_BUTTON_TEXT));
+        btn.href = "#";
+        btn.focus();
+        btn.onclick = function() { removeCustomAlert();return false; }
+    
+        alertObj.style.display = "block";
+    
+    }
+    
+    removeCustomAlert() {
+        document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
+    }
+
+} 
